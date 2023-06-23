f6d23f2863041d98e944d6feae1dad28b6a48369
Nicoinlas <49500208+Nicoinlas@users.noreply.github.com>
2023-06-23 17:09:56 +0200

    Update 230623_Short_Analysis_Multiple_Uploader_v01.py

--- 230623_Short_Analysis_Multiple_Uploader_v01.py
+++ 230623_Short_Analysis_Multiple_Uploader_v01.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-import streamlit as st
-import pandas as pd
-from io import BytesIO
-import zipfile
-
-
-# In[2]:
-
-
 name = st.text_input("File Name")
 date = st.text_input("Today's date i.e. 231231")
 
@@ -26,8 +11,7 @@ def combinesdds(xlsxs):
     dfs = {name: [] for name in sheet_names}
     for xlsx in xlsxs:
         for sheet_name in sheet_names:
-            df = pd.read_excel(xlsx, sheet_name=sheet_names)
-            df = xlsx.parse(sheet_name)
+            df = pd.read_excel(xlsx, sheet_name=sheet_name)
             dfs[sheet_name].append(df)
     return dfs
 
@@ -37,34 +21,23 @@ def zipsdd_csvs(dfs,name,date):
     zip_io = BytesIO()
     with zipfile.ZipFile(zip_io, mode='w') as zipped_files:
         for sheet_name in sheet_names:
+            combined_df = pd.concat(dfs[sheet_name])  # concatenate the dataframes first
             file_name = str(date + " " + name + "_" + sheet_name + ".csv")
-            csv_data = dfs[sheet_name].to_csv(index=False, encoding="utf-8-sig")
+            csv_data = combined_df.to_csv(index=False, encoding="utf-8-sig")
             zipped_files.writestr(file_name, csv_data.encode('utf-8-sig'))
     zip_io.seek(0)
     return zip_io
 
 btn = st.button("Download CSVs")
+
 if btn:
-        dfs =  combinesdds(xlsxs)
-        zip_io = zipsdd_csvs(dfs,name,date)
-        tmp_download_link = st.download_button(
-            label='Download CSVs',
-            data=zip_io,
-            file_name='dataframes.zip',
-            mime='application/zip'
+    dfs =  combinesdds(xlsxs)
+    zip_io = zipsdd_csvs(dfs,name,date)
+    tmp_download_link = st.download_button(
+        label='Download CSVs',
+        data=zip_io,
+        file_name='dataframes.zip',
+        mime='application/zip'
         )
-
-
-
-
-# In[15]:
-
-
-
-
-
-# In[17]:
-
-
 
 
