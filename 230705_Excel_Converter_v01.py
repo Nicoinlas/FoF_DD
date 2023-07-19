4812c36131f9ce8e415c973b21de970ddadfb5e3
Nicoinlas <49500208+Nicoinlas@users.noreply.github.com>
2023-07-19 17:47:58 +0200

    Update 230705_Excel_Converter_v01.py

--- 230705_Excel_Converter_v01.py
+++ 230705_Excel_Converter_v01.py
@@ -76,12 +76,12 @@ def zipsdd_csvs(dfs,name,date, sheet_names):
                 # Format the datetime column to the desired format
                 df[col] = df[col].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
                       
-            if "Vintage PQ" in df.columns:
+             if "Vintage PQ" in df.columns:
                 # Fill NaNs with a dummy value
                 df["Vintage PQ"].fillna(-1, inplace=True)
                 
                 # Convert to integer, then to string, and replace the dummy value with NaN
-                df["Vintage PQ"] = df["Vintage PQ"].astype(int).astype(str).replace('-1', np.nan)
+                df["Vintage PQ"] = df["Vintage PQ"].apply(lambda x: str(int(x)) if x != -1 else np.nan)
 
             file_name = str(date + " " + name + "_" + sheet_name + ".csv")
             csv_data = df.to_csv(index=False, encoding="utf-8-sig")
